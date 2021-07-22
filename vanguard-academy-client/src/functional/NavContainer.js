4a1ad9a41b898ff52c14ec3f99c43ee74f6d07e3
jesus_garcia <voli.scorp13@gmail.com>
2021-07-22 09:02:29 -0500

    added

--- vanguard-academy-client/src/functional/NavContainer.js
+++ vanguard-academy-client/src/functional/NavContainer.js
@@ -14,31 +14,31 @@ const NavContainer = ({isAuthenticated,setIsAuthenticated}) => {
                     {(isAuthenticated.isLoggedIn)? 
                             [(isAuthenticated.role === 'stu')? 
                             <div className='nav-comp' id='nav-detail-stu'>
-                                <Link className='nav-links' to='/profile'> Profile </Link>
-                                <Link className='nav-links' to='/profile/details'> Edit Profile </Link>
-                                <Link className='nav-links' to='/learnForm'> Learning Form </Link>
-                                <Link className='nav-links'> Schedule </Link>
-                                <Link className='nav-links'> Assignments </Link>
-                                <Link className='nav-links' to='/courses'> Request Course </Link>
+                                <div><Link className='nav-links' to='/profile'> Profile </Link></div>
+                                <div><Link className='nav-links' to='/profile/details'> Edit Profile </Link></div>
+                                <div><Link className='nav-links' to='/learnForm'> Learning Form </Link></div>
+                                <div><Link className='nav-links'> Schedule </Link></div>
+                                <div><Link className='nav-links'> Assignments </Link></div>
+                                <div><Link className='nav-links' to='/courses'> Request Course </Link></div>
                                 <button onClick={handleLogout}> Log Out </button>
                             </div>
                             : 
                             <div className='nav-comp' id='nav-detail-ins'>
-                                <Link className='nav-links' to='/profile'> Profile </Link>
-                                <Link className='nav-links' to='/profile/details'> Edit Profile </Link>
-                                <Link className='nav-links'> Classes </Link>
-                                <Link className='nav-links' to='/classAssignments'> Class Assignments </Link>
-                                <Link className='nav-links'> Assignment Data Table </Link>
+                                <div><Link className='nav-links' to='/profile'> Profile </Link></div>
+                                <div><Link className='nav-links' to='/profile/details'> Edit Profile </Link></div>
+                                <div><Link className='nav-links'> Classes </Link></div>
+                                <div><Link className='nav-links' to='/classAssignments'> Class Assignments </Link></div>
+                                <div><Link className='nav-links'> Assignment Data Table </Link></div>
                                 <button onClick={handleLogout}> Log Out </button>
                             </div>
                             ]
                     : 
                         <div className='nav-comp' id='nav-detail'>
-                            <Link className='nav-links' to='/home'> Home</Link>
-                            <Link className='nav-links' to='/about'> About </Link>
-                            <Link className='nav-links' to='/welcome'> Welcome </Link>            
-                            <Link className='nav-links' to='/admissions'> Admissions </Link>
-                            <Link className='nav-links' to='/login'> Log In</Link>
+                            <div><Link className='nav-links' to='/home'> Home</Link></div>
+                            <div><Link className='nav-links' to='/about'> About </Link></div>
+                            <div><Link className='nav-links' to='/welcome'> Welcome </Link></div>            
+                            <div><Link className='nav-links' to='/admissions'> Admissions </Link></div>
+                            <div><Link className='nav-links' to='/login'> Log In</Link></div>
                         </div>
                     }           
         </div>
